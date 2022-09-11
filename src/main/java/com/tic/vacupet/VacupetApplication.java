ace3d5006bdee4f5be9976d7fa2bba7378fe2af7
Carlos Pavajeau <cantte098@gmail.com>
2022-09-11 12:08:59 -0500

    feat: first commit

---
+++ src/main/java/com/tic/vacupet/VacupetApplication.java
@@ -0,0 +1,13 @@
+package com.tic.vacupet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacupetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacupetApplication.class, args);
+	}
+
+}
