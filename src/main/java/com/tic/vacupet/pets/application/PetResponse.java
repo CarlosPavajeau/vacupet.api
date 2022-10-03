7089c406d67551299d43b6a4ff30d9c0ad63a9d7
Carlos Pavajeau <cantte098@gmail.com>
2022-10-03 09:39:43 -0500

    feat: load pet vaccines

--- src/main/java/com/tic/vacupet/pets/application/PetResponse.java
+++ src/main/java/com/tic/vacupet/pets/application/PetResponse.java
@@ -1,4 +1,18 @@
 package com.tic.vacupet.pets.application;
 
-public record PetResponse(Long id, String breed, String name, String birthDate, boolean hasChip, boolean hasMedicalRecord, boolean hasAllergies, String observations, OwnerResponse owner) {
+import com.tic.vacupet.vaccines.application.VaccineResponse;
+
+import java.util.List;
+
+public record PetResponse(
+        Long id,
+        String breed,
+        String name,
+        String birthDate,
+        boolean hasChip,
+        boolean hasMedicalRecord,
+        boolean hasAllergies,
+        String observations,
+        OwnerResponse owner,
+        List<VaccineResponse> vaccines) {
 }
